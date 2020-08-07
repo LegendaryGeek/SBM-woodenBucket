da5dc30f181fedc27910280bf943dc9e5490087f
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-08-07 17:22:59 -0400

    make the material not null

--- src/main/java/com/builtbroken/woodenbucket/BucketTypes.java
+++ src/main/java/com/builtbroken/woodenbucket/BucketTypes.java
@@ -5,17 +5,20 @@ import net.minecraft.item.ItemStack;
 
 public enum BucketTypes
 {
-    OAK,
-    ACACIA,
-    BIRCH,
-    JUNGLE,
-    SPRUCE,
-    BIG_OAK,
-    CHARRED;
+    OAK(new BucketMaterial("oak", "oak")),
+    ACACIA(new BucketMaterial("acacia", "acacia")),
+    BIRCH(new BucketMaterial("birch", "birch")),
+    JUNGLE(new BucketMaterial("jungle", "jungle")),
+    SPRUCE(new BucketMaterial("spruce", "spruce")),
+    BIG_OAK(new BucketMaterial("bigoak", "bigoak")),
+    CHARRED(new BucketMaterial("charred", "charred"));
 
     public BucketMaterial material;
 
-    public ItemStack getBucket()
+    BucketTypes(BucketMaterial bucketMaterial) {
+    	this.material = bucketMaterial;
+	}
+	public ItemStack getBucket()
     {
     	return new ItemStack(new WoodBucketItem(new Properties(), material));
     }
